75af8f69eb114a69810da69f3b2b1e95dfe42d77
Yashaswe <amatyayasu@gmail.com>
2022-01-07 13:04:22 +0545

    form validation

--- src/component/Newsletter.jsx
+++ src/component/Newsletter.jsx
@@ -1,6 +1,24 @@
 import FullName from "../img/icons/fullname.png";
 import Email from "../img/icons/email.png";
+import { useState } from "react";
 const Newsletter = () => {
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  function subscribed() {
+    if (email.length === 0 || fullname.length === 0) {
+      window.alert("Please fill all the required field");
+    } else if (
+      !email
+        .toLowerCase()
+        .match(
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        )
+    ) {
+      window.alert("Enter correct email");
+    } else {
+      window.alert("Thank you for subscribing");
+    }
+  }
   return (
     <div className="newsletter">
       <h2>Subscribe to our newsletter</h2>
@@ -17,6 +35,7 @@ const Newsletter = () => {
               id="fullName"
               placeholder="Full Name"
               required="required"
+              onChange={(e) => setFullname(e.target.value)}
             ></input>
           </div>
           <div className="input_container">
@@ -26,14 +45,13 @@ const Newsletter = () => {
               placeholder="Email"
               className="email"
               required="required"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
             ></input>
           </div>
         </div>
-        <button
-          type="submit"
-          className="subscribe"
-          onClick={() => window.alert("Thank you for subscribing")}
-        >
+        <button type="submit" className="subscribe" onClick={subscribed}>
           Subscribe
         </button>
       </form>
